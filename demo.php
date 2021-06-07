a90e4695c6ceb3edc077aabf672aaa9c8de67f1a
daxitasonar02 <84956249+daxitasonar02@users.noreply.github.com>
2021-06-07 16:30:58 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        
+        
+        
+           your name is:
+            <?php  $post["txt1"];
+            ?>   <br>
+            your age is:
+            
+            <?php  $post["txt2"];
+            ?> 
+            
+            
+            
+    </body>
+    
+    
+    
+    
+</html>
