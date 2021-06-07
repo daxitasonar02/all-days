622e798436e03a3aba503b63b6a87156105cab60
daxitasonar02 <84956249+daxitasonar02@users.noreply.github.com>
2021-06-07 16:33:19 +0530

    Add files via upload

---
+++ Master.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>WINE-SHOP</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<table width="95%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="left" valign="top"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td align="left" valign="top" class="header"><table width="405" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="left" valign="top"><a href="#" title="logo"><img src="images/logo.jpg" alt="logo" width="192" height="22" border="0" class="logo" /></a></td>
+          </tr>
+          <tr>
+            <td align="left" valign="top">
+                <p class="cap">New Luxury Wine From India </p> </td>
+			
+          </tr>
+          <tr>
+            <td align="left" valign="top" class="morebutton"><a href="#" title="Read More">Read More</a></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="left" valign="top" class="navigationbg"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+     <?php
+     include './thempart/menu.php';
+     ?>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="left" valign="top"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td align="left" valign="top"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td align="left" valign="top"><table width="315" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="left" valign="top" class="logintext">Member Login</td>
+              </tr>
+              <tr>
+                <td align="left" valign="top"><form id="form1" name="form1" method="post" action="">
+                  <table width="315" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td align="left" valign="top" class="text">Username</td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><label>
+                        <input name="textfield" type="text" class="textfield" />
+                      </label></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top" class="text">Password</td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><label>
+                        <input name="textfield2" type="password" class="textfield" />
+                      </label></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><table width="315" border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                          <td width="155" align="left" valign="top" class="forgetpassword">Forget Password?</td>
+                          <td align="left" valign="top" class="click"><a href="#" title="Click Here">Click Here</a></td>
+                        </tr>
+                      </table></td>
+                    </tr>
+                  </table>
+                                </form>
+                </td>
+              </tr>
+             
+              <tr>
+                <td align="left" valign="top">&nbsp;</td>
+              </tr>
+             </table></td>
+            <td width="585" align="left" valign="top"><table width="585" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                  <td align="left" valign="top" class="welcome"><strong><h3>WELCOME TO OUR SHOP</h3></strong>
+              </tr>
+              <tr>
+                <td align="left" valign="top"><p class="caption"><a href="#">Pellentesque nisi elit, pharetra ac, tristique vestibulum porta ut</a></p>
+				<p class="welcometext">Nullam lectus nibh, mollis ac, tincidunt ac, sollicitudin eu, neque. Morbi tincidunt, neque non <br /><u>pulvinar luctus, nisi lorem faucibus dui, sed molestie justo</u> metus eget libero.Vestibulum arcu <br />
+				  massa, sollicitudin vel, mattis vel, tincidunt in arcu <u>Donec nec nisl. Phasellus pulvinar nulla ut <br />
+				  neque. Cras placerat, lectus at cursus lacinia, mi risus tempor massa.</u></p>
+				</td>
+				 </tr>
+              <tr>
+                <td align="left" valign="top"><table width="585" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                      <td align="right" valign="top" class="learnmore"><a href="#" title="Learn more">Learn more</a></td>
+                  </tr>
+                  <tr>
+                      <td align="left" valign="top" class="welcome">Product</td>
+					
+					
+                  </tr>
+                  <tr>
+                    <td align="left" valign="top"><table width="585" border="0" cellspacing="0" cellpadding="0">
+                      <tr>
+                        <td align="left" valign="top" class="pading"><table width="466" border="0" align="left" cellpadding="0" cellspacing="0">
+                          <tr>
+                            <td width="207" align="left" valign="top"><table width="207" border="0" align="left" cellpadding="0" cellspacing="0">
+                              <tr>
+                                <td align="left" valign="top"><a href="#" title="img-1"><img src="images/img-1.jpg" alt="img-1" width="200" height="100" border="0" class="featureimg" /></a>
+              </td>
+                              </tr>
+                              
+                            </table></td>
+                            <td width="" align="left" valign="top">&nbsp;</td>
+                            <td align="left" valign="top"><table width="207" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td align="left" valign="top"><a href="#" title="img-2"><img src="images/img-2.jpg" alt="img-2" width="200" height="100" border="0" class="featureimg" /></a>
+           </td>
+                              </tr>
+                              
+                            </table></td>
+                          </tr>
+                          <tr>
+                            <td height="22" align="left" valign="top">&nbsp;</td>
+                            <td align="left" valign="top">&nbsp;</td>
+                            <td align="left" valign="top">&nbsp;</td>
+                          </tr>
+                          <tr>
+                            <td align="left" valign="top"><table width="207" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td align="left" valign="top"><a href="#" title="img-3"><img src="images/img-3.jpg" alt="img-3" width="200" height="100" border="0" class="featureimg" /></a>
+              </td>
+                              </tr>
+                              
+                            </table></td>
+                            <td align="left" valign="top">&nbsp;</td>
+                            <td align="left" valign="top"><table width="207" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td align="left" valign="top"><a href="#" title="img-4"><img src="images/img-4.jpg" alt="img-4" width="200" height="100" border="0" class="featureimg" /></a>
+              </td>
+                              </tr>
+                              
+                            </table></td>
+                          </tr>
+                        </table></td>
+                      </tr>
+                      
+                    </table></td>
+                  </tr>
+                </table></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td align="left" valign="top"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+          
+          <tr>
+            <td align="center" valign="top"><p class="copyright"> 2021.WEBSITE MANAGE BY DAXITA.<br />
+
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
